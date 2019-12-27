633080d8f98c814f560700141c8ee7defbdbc643
cuong <cuong.lam@twentyci.asia>
2019-12-27 18:06:43 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import Routes from "./Router";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+        <Navbar>
+            <Navbar.Brand>
+                <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+        </Navbar>
+        <Routes/>
     </div>
   );
 }
